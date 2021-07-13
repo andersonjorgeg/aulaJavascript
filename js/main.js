65582f39c88714ee00b76f00b452b921dbefc51f
Anderson Jorge <anderson.jorge33@gmail.com>
2021-07-13 09:02:14 -0300

    aula 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nome = "Anderson Jorge";
+let n1 = 5;
+let n2 = 10;
+let frase = "Japão é o melhor time do mundo";
+/* alert(`${nome} tem ${idade} anos`); */
+
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1*n2)
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"))
